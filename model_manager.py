9ef8f163d4c45ae6c89cb8e779081972e457616f
Franciszek Fabinski <franciszek.fabinski@pbs.pl>
2025-08-18 10:19:06 +0200

    feat(model): multilingual paraphrase-MiniLM L12 using HF transformers

--- model_manager.py
+++ model_manager.py
@@ -3,12 +3,15 @@ import torch
 
 
 def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output[0]
+    token_embeddings = model_output[
+        0
+    ]  # First element of model_output contains all token embeddings
+    attention_mask = attention_mask.to(token_embeddings.device)
     input_mask_expanded = (
         attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     )
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
-        input_mask_expanded.sum(1)
+        input_mask_expanded.sum(1), min=1e-9
     )
 
 
@@ -28,15 +31,20 @@ class ModelManager:
         self.messages = [config["system_prompt"]]
         self.device = config["device"]
         self.max_new_tokens = config["max_new_tokens"]
-        self.categories = self.generate_categories(config["categories"])
+        self.categories = None
 
         self.model.to(self.device)
 
     def prompt_model(self, prompt: str):
-        all_sentences = [prompt].extend(self.categories)
+        all_sentences = self.categories.copy()
+        all_sentences.append(prompt)
+        print(
+            f"typ kategorii: {type(self.categories)}, typ prompta: {type(prompt)}, typ all_categories: {type(all_sentences)}"
+        )
         encoded_input = self.tokenizer(
             all_sentences, padding=True, truncation=True, return_tensors="pt"
         )
+        encoded_input = {k: v.to(self.device) for k, v in encoded_input.items()}
         with torch.no_grad():
             model_output = self.model(**encoded_input)
 
